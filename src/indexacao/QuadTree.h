fbebbaba98a488d0a209aa81631543c8152d2a25
lisianeof1 <lisianeo.f@gmail.com>
2026-07-05 21:10:11 -0300

    Remove Ponto and Retangulo structs
    Removed Ponto and Retangulo structs from QuadTree.h

--- src/indexacao/QuadTree.h
+++ src/indexacao/QuadTree.h
@@ -5,25 +5,6 @@
 
 using namespace std;
 
-
-struct Ponto{
-    double x;
-    double y;
-
-    Ponto(double x = 0, double y = 0);
-};
-
-struct Retangulo{
-    double x;
-    double y;
-    double largura;
-    double altura;
-
-    Retangulo(double x = 0, double y = 0, double largura = 0, double altura = 0);
-
-    bool tem_ponto(const Ponto &p) const;
-};
-
 class QuadTree{
     private:
     Retangulo limite;
