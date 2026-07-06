72cfcb14d40e55fe9fc2519f934cb41665b3aa1d
lisianeof1 <lisianeo.f@gmail.com>
2026-07-05 22:59:06 -0300

    Refactor SVG drawing logic in VisualizadorSVG

--- src/visualizacao/VisualizadorSVG.cpp
+++ src/visualizacao/VisualizadorSVG.cpp
@@ -1,26 +1,23 @@
 #include "VisualizadorSVG.h"
+#include <iostream>
 
 using namespace std;
 
 void VisualizadorSVG::desenharQuadTree(ofstream &arquivo, const QuadTree *qt)
 {
-    if (qt == nullptr)
-        return;
-
+    if (qt == nullptr) return;
     const Retangulo &r = qt->getLimite();
 
-    arquivo << "<rect x=\"" << r.x << "\" y=\"" << r.y
-            << "\" width=\"" << r.largura
-            << "\" height=\"" << r.altura
+    arquivo << "<rect x=\"" << r.inferiorEsquerdo.x << "\" y=\"" << r.inferiorEsquerdo.y
+            << "\" width=\"" << r.largura()
+            << "\" height=\"" << r.altura()
             << "\" fill=\"none\" stroke=\"olive\" stroke-width=\"1\" />\n";
 
-    for (const Ponto &p : qt->getPontos())
-    {
+    for (const Ponto &p : qt->getPontos()) {
         desenharPonto(arquivo, p);
     }
 
-    if (qt->estaDividida())
-    {
+    if (qt->estaDividida()) {
         desenharQuadTree(arquivo, qt->getNordeste());
         desenharQuadTree(arquivo, qt->getNoroeste());
         desenharQuadTree(arquivo, qt->getSudeste());
@@ -38,26 +35,28 @@ void VisualizadorSVG::desenharPonto(ofstream &arquivo, const Ponto &ponto)
 void VisualizadorSVG::exportar(const string &nomeArquivo, const QuadTree &qt)
 {
     ofstream arquivo(nomeArquivo);
-
-    if (!arquivo.is_open())
+    if (!arquivo.is_open()) {
+        cout << "[-] ERRO: Nao foi possivel salvar em '" << nomeArquivo << "'. A pasta 'resultados' existe neste diretorio?\n";
         return;
-
+    }
     const Retangulo &limite = qt.getLimite();
 
     arquivo << "<svg xmlns=\"http://www.w3.org/2000/svg\" "
-            << "width=\"" << limite.largura << "\" "
-            << "height=\"" << limite.altura << "\" "
-            << "viewBox=\"" << limite.x << " " << limite.y << " "
-            << limite.largura << " " << limite.altura << "\">\n";
+            << "width=\"" << limite.largura() << "\" "
+            << "height=\"" << limite.altura() << "\" "
+            << "viewBox=\"" << limite.inferiorEsquerdo.x << " " << limite.inferiorEsquerdo.y << " "
+            << limite.largura() << " " << limite.altura() << "\">\n";
 
-    arquivo << "<rect x=\"" << limite.x << "\" y=\"" << limite.y
-            << "\" width=\"" << limite.largura
-            << "\" height=\"" << limite.altura
+    arquivo << "<rect x=\"" << limite.inferiorEsquerdo.x << "\" y=\"" << limite.inferiorEsquerdo.y
+            << "\" width=\"" << limite.largura()
+            << "\" height=\"" << limite.altura()
             << "\" fill=\"lightgreen\" stroke=\"black\" stroke-width=\"2\" />\n";
 
     desenharQuadTree(arquivo, &qt);
 
     arquivo << "</svg>\n";
+    arquivo.close();
+}
 
     arquivo.close();
 }
